075cd50e4899925eb61dc5e90c9168d6f52f9cce
SanvedM <sanvedmayangade@gmail.com>
2026-04-05 21:30:40 +0530

    fix logout issue

--- ecom-front-end/src/components/Navbar.jsx
+++ ecom-front-end/src/components/Navbar.jsx
@@ -243,7 +243,6 @@ export default function Navbar() {
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
-    window.location.reload();
   };
 
   const scrollToCategories = () => {
